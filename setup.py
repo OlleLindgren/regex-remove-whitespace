2dce78862d360647f46d08afdc9af8d480c2be6a
OlleLindgren <lindgrenolle@live.se>
2022-10-01 17:14:53 +0200

    Initial commit, release version 1

---
+++ setup.py
@@ -0,0 +1,8 @@
+"""rmspace installer"""
+import setuptools
+
+setuptools.setup(
+    name="rmspace",
+    version="1",
+    description="Remove trailing whitespace from code and other plain-text files.",
+)
